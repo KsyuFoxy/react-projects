d61695f0291e431483b06a99056ccb870754ae51
Oksana Kondratiuk <ksyu@web.de>
2017-03-28 17:39:11 +0200

    add BG-image, edit Footer element

--- recipes_page_2/src/Header_Footer.js
+++ recipes_page_2/src/Header_Footer.js
@@ -10,9 +10,20 @@ export class Header extends React.Component {
   };
 }
 export class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'marginTop': 132
+        }
+    }
     render() {
+        // var screenHeight = window.screen.availHeight;
+        // var marginTop = this.state.style.marginTop;
+        //
+        // console.log(marginTop)
+
       return (
-        <div className='footer'>
+        <div className='footer' style={{marginTop: this.state.value + 'px'}}>
               <p>@ Copyright 2017 All rights reserved</p>
         </div>
       )
